942db38f66fbfb7892f26eb547644bd18792e96e
marcusp619 <marcusp619@gmail.com>
2018-12-27 19:13:08 -0700

    Add form and card style

--- src/components/NoteItem/index.js
+++ src/components/NoteItem/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Button, Header, Container } from 'semantic-ui-react'
+import HeaderSubHeader from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
 
-const NoteItem = ({ title, noteDescription, id, removeNote }) => (
-  <div className="note-item" key={id}>
-    <h1>{title}</h1>
-    <p>{noteDescription}</p>
-    <button onClick={() => removeNote(id)}>Remove Note</button>
-  </div>
+const NoteItem = ({ title, noteDescription, id, tag, removeNote }) => (
+  <Container style={containerStyle} key={id}>
+    <Header color="orange" size="huge">
+      {title}
+      <Header.Subheader>Tag: {tag}</Header.Subheader>
+    </Header>
+    <p style={pStyle}>Note: {noteDescription}</p>
+    <Button secondary onClick={() => removeNote(id)}>Remove Note</Button>
+  </Container>
 );
+
+const containerStyle = {
+  padding: '1rem',
+}
+
+const pStyle = {
+  fontSize: '1.4rem',
+}
 
 export default NoteItem;
--- src/components/NoteList/index.js
+++ src/components/NoteList/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import uuidv4 from 'uuid/v4'
 import NoteItem from '../NoteItem/';
+import { Container, Segment } from 'semantic-ui-react'
 
 const NoteList = ({ notes, removeNote }) => {
   if (notes) {
     const noteItems = notes.map(note => (
-      <NoteItem {...note} removeNote={removeNote} key={uuidv4()} />
+      <Segment raised piled >
+        <NoteItem {...note} removeNote={removeNote} key={uuidv4()} />
+      </Segment>
     ));
-    return <div className="todo-list-container">{noteItems}</div>;
+
+    return <Container style={containerStyle}>{noteItems}</Container>;
   }
 };
+
+const containerStyle = {
+  boxShadow: 'rgb(204, 204, 204) 0px 1px 2px',
+  background: '#f5f5f5',
+  marginBottom: '1rem',
+  overflow: 'auto',
+  maxHeight: '70vh'
+}
 
 export default NoteList;
